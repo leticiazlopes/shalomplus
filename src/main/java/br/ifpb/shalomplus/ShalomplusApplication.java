b8ee20aea4daf0fc23b3dde5c65e8ee726d91be8
Leticia Lopes Lima <leticiazlzlima@gmail.com>
2025-11-24 13:21:43 -0300

    First Commit

---
+++ src/main/java/br/ifpb/shalomplus/ShalomplusApplication.java
@@ -0,0 +1,13 @@
+package br.ifpb.shalomplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShalomplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShalomplusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ifpb/shalomplus/ShalomplusApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ifpb.shalomplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShalomplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
